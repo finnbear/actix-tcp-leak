93c9b3e51e9723a6f8c9642173f3aa3f83249ae7
Finn Bear <finnbearlabs@gmail.com>
2021-10-13 18:27:43 -0700

    Isolate SSL as the problem.

--- README.md
+++ README.md
@@ -3,15 +3,10 @@ Reproducible [TCP connection leak](https://github.com/actix/actix-net/issues/351
 
 ## Setup
 
-1. Create a VPS with Debian 11 and your SSH public key
-2. Add its IP address to the Makefile
-3. Run `make provision`
-4. Run `make ssh` or otherwise connect to the VPS
-5. Run `./tcp-leak` on the VPS
-6. Install `wrk` and run `make wrk`
-7. Disconnect your internet connection (I tested on a laptop with WiFi, and turning off the WiFi was sufficient)
-8. Observe that some or all of the inflight TCP connections are permanently leaked
-9. As an alternative to 6-8, observe that ~1 out of every 100 real-end-user connection to the VPS public IP will leak
+1. Install `netstat` and `libopenssl-devel` (and whatever else is required to compile this)
+2. `cargo run`
+3. Watch the `CLOSE_WAIT` connections steadily leak.
+4. On a laptop on the same network, navigate to `https://IP_OF_RUNNING_PROGRAM:4443`, click through the SSL warning, wait until you see "Hello World!," and then turn off your WiFi to leak one `ESTABLISHED` connection.
 
 ## Note
 
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,34 @@
 #![feature(async_closure)]
 
-use actix_http::KeepAlive;
 use actix_web::{web, App, HttpResponse, HttpServer};
+use openssl::ssl::{SslConnector, SslMethod, SslVerifyMode};
 use rustls::internal::pemfile::{certs, pkcs8_private_keys};
 use rustls::{NoClientAuth, ServerConfig};
-use std::io::BufReader;
+use std::collections::BTreeMap;
+use std::io::{BufReader, Write};
+use std::net::TcpStream;
 use std::process::Command;
 use std::time::Duration;
-use std::{io, thread};
+use std::{io, mem, thread};
 use tokio::time::sleep;
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
     let mut counter = 0;
     thread::spawn(move || loop {
-        println!("{}, {}", counter, established_connection_count());
+        println!("{}, {:?}", counter, connection_counts());
         counter += 1;
         thread::sleep(Duration::from_secs(1));
+
+        leak();
     });
 
     let app = move || {
         App::new().service(web::resource("/").route(web::get().to(async || {
-            sleep(Duration::from_secs(1)).await;
+            sleep(Duration::from_secs(2)).await;
             HttpResponse::Ok().body("Hello World!")
         })))
     };
-
-    let mut counter = 0;
-    thread::spawn(move || loop {
-        let output = Command::new("netstat").arg("-natp").output().unwrap();
-        let count = std::str::from_utf8(&output.stdout)
-            .unwrap()
-            .split('\n')
-            .filter(|s| s.contains("ESTABLISHED") && s.contains("tcp-leak"))
-            .count();
-        println!("{}, {}", counter, count);
-        counter += 1;
-        thread::sleep(Duration::from_secs(1));
-    });
 
     let mut config = ServerConfig::new(NoClientAuth::new());
     let cert_chain = certs(&mut BufReader::new(&include_bytes!("example.crt")[..])).unwrap();
@@ -46,17 +37,61 @@ async fn main() -> io::Result<()> {
     config.set_single_cert(cert_chain, keys.remove(0)).unwrap();
 
     HttpServer::new(app)
-        .keep_alive(KeepAlive::Timeout(5))
-        .bind_rustls("0.0.0.0:443", config)?
+        .bind_rustls("0.0.0.0:4443", config)?
         .run()
         .await
 }
 
-fn established_connection_count() -> usize {
+fn leak() {
+    let stream = TcpStream::connect("127.0.0.1:4443").unwrap();
+
+    let mut builder = SslConnector::builder(SslMethod::tls()).unwrap();
+    builder.set_verify(SslVerifyMode::NONE);
+    let connector = builder.build();
+
+    let mut stream = connector.connect("localhost", stream).unwrap();
+    stream.ssl_write(&b"prepare to die, actix web"[..]).unwrap();
+    stream.flush().unwrap();
+
+    // Removing this avoids the leak, but the client cannot be trusted.
+    mem::forget(stream);
+}
+
+fn connection_counts() -> BTreeMap<String, usize> {
+    let mut ret = BTreeMap::new();
+
     let output = Command::new("netstat").arg("-natp").output().unwrap();
-    std::str::from_utf8(&output.stdout)
+    for s in std::str::from_utf8(&output.stdout)
         .unwrap()
         .split('\n')
-        .filter(|s| s.contains("ESTABLISHED") && s.contains("tcp-leak"))
-        .count()
+        .filter(|s| {
+            s.contains("tcp-leak") || s.contains("target/debug") || s.contains("target/release")
+        })
+    {
+        let mut key = None;
+        for potential in [
+            "LISTEN",
+            "SYN_SENT",
+            "SYN_RECEIVED",
+            "ESTABLISHED",
+            "FIN_WAIT_1",
+            "FIN_WAIT_2",
+            "CLOSE_WAIT",
+            "CLOSING",
+            "LAST_ACK",
+            "TIME_WAIT",
+            "CLOSED",
+        ] {
+            if s.contains(potential) {
+                key = Some(potential);
+                break;
+            }
+        }
+
+        if let Some(key) = key {
+            *ret.entry(key.to_owned()).or_insert(0) += 1;
+        }
+    }
+
+    ret
 }
